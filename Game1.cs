730b949a18996be9b71679511198a83af476c4ae
CriticalDesign <100966505+CriticalDesign@users.noreply.github.com>
2023-05-21 20:24:57 -0400

    Many updates, have been forgetting to commit. Starting to work on code efficiencey.

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Collections.Generic;
+using System;
 
 namespace GremlinWalks
 {
@@ -8,19 +11,34 @@ namespace GremlinWalks
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Song _music;
 
-        private Gremlin _gremlin;
+        private Random _rng;
+
+        private Gremlin _gretl, _kurt, _liesl, _marta, _louisa, _brigitta, _friedrich;
+
+        private List<Gremlin> _gremlins;
+
+        int _chaseTarget;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.ApplyChanges();
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _gremlins = new List<Gremlin>();
+            _rng = new Random();
+            _chaseTarget = -1;
+
 
             base.Initialize();
         }
@@ -28,7 +46,38 @@ namespace GremlinWalks
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            _gremlin = new Gremlin(Content.Load<Texture2D>("gremlinSprites"), Content.Load<SpriteFont>("gremlinFont"), "Gretl", "I'm pacing.");
+
+            _music = Content.Load<Song>("chiptune");
+            MediaPlayer.Play(_music);
+
+            _louisa = new Gremlin(Content.Load<Texture2D>("gremlinSprites"), Content.Load<SpriteFont>("gremlinFont"), "Louisa", "I'm chasing.", "chase", Color.Yellow);
+            _louisa.SetStartPosition(new Vector2(50, 400));
+            _gretl = new Gremlin(Content.Load<Texture2D>("gremlinSprites"), Content.Load<SpriteFont>("gremlinFont"), "Gretl", "I'm pacing.", "pace", Color.White);
+            _gretl.SetStartPosition(new Vector2(100, 100));
+            _kurt = new Gremlin(Content.Load<Texture2D>("gremlinSprites"), Content.Load<SpriteFont>("gremlinFont"), "Kurt", "I'm idle.", "idle", Color.Orange);
+            _kurt.SetStartPosition(new Vector2(550, 200));
+            _liesl = new Gremlin(Content.Load<Texture2D>("gremlinSprites"), Content.Load<SpriteFont>("gremlinFont"), "Liesl", "I'm circling.", "circle", Color.Red);
+            _liesl.SetStartPosition(new Vector2(665, 200));
+            _liesl.SetCircle(10f);
+            _marta = new Gremlin(Content.Load<Texture2D>("gremlinSprites"), Content.Load<SpriteFont>("gremlinFont"), "Marta", "I'm random.", "random", Color.Pink);
+            _marta.SetStartPosition(new Vector2(700, 500));
+            _brigitta = new Gremlin(Content.Load<Texture2D>("gremlinSprites"), Content.Load<SpriteFont>("gremlinFont"), "Brigitta", "I'm bouncing.", "bounce", Color.Green);
+            _brigitta.SetStartPosition(new Vector2(150, 400));
+            _brigitta.SetDirection(new Vector2(_rng.Next(0,800), _rng.Next(0,600)));
+            _friedrich = new Gremlin(Content.Load<Texture2D>("gremlinSprites"), Content.Load<SpriteFont>("gremlinFont"), "Friedrich", "I'm following a path.", "path", Color.Purple);
+            _friedrich.SetStartPosition(new Vector2(400, 400));
+            _friedrich.SetPath();
+
+
+            _gremlins.Add(_louisa);
+            _gremlins.Add(_gretl);
+            _gremlins.Add(_kurt);
+            _gremlins.Add(_liesl);
+            _gremlins.Add(_marta);
+            _gremlins.Add(_brigitta);
+            _gremlins.Add(_friedrich); 
+
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,7 +85,17 @@ namespace GremlinWalks
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            _gremlin.Update(gameTime);
+            foreach(Gremlin gremlin in _gremlins)
+            {
+                gremlin.Update(gameTime);
+            }
+
+            if (_chaseTarget == -1 || Vector2.Distance(_louisa.GetPosition(), _gremlins[_chaseTarget].GetPosition()) < 75)
+            {
+                _chaseTarget = _rng.Next(1, _gremlins.Count);
+                _louisa.SetChatter("\"I'm chasing.\" (" + _gremlins[_chaseTarget].GetName() + ")");
+            }
+            _louisa.SetChaseTarget(_gremlins[_chaseTarget].GetPosition());
 
             base.Update(gameTime);
         }
@@ -45,7 +104,10 @@ namespace GremlinWalks
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            _gremlin.Draw(_spriteBatch);
+            foreach (Gremlin gremlin in _gremlins)
+            {
+                gremlin.Draw(_spriteBatch);
+            }
 
             base.Draw(gameTime);
         }
--- Gremlin.cs
+++ Gremlin.cs
@@ -2,6 +2,8 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
 
 namespace GremlinWalks
 {
@@ -10,43 +12,104 @@ namespace GremlinWalks
         private Texture2D _gremlinSprite;
         private SpriteFont _gremlinFont;
         private int _rows, _cols, _currentFrame, _endCol, _animRow;
-        private Vector2 _position, _direction;
-        private float _speed, _scale;
+        private Vector2 _position, _chaseTarget, _direction;
+        private float _speed, _scale, _circularAngle;
         private string _name, _chatter;
+        private string _moveType;
+        private Color _color;
 
 
         private bool _paceLeft;
 
+        private List<Vector2> _path;
+        private int _destination;
+
         int timeSinceLastFrame = 0;
         int millisecondsPerFrame = 100;
 
-        public Gremlin(Texture2D gremlinSprite, SpriteFont gremlinFont, string name, string chatter)
+        int randomHTimeSinceLastFrame = 0;
+        int randomHMillisecondsPerFrame = 2000;
+        int randomVTimeSinceLastFrame = 0;
+        int randomVMillisecondsPerFrame = 2000;
+        int randomDeltaX = 0;
+        int randomDeltaY = 0;
+
+        public Gremlin(Texture2D gremlinSprite, SpriteFont gremlinFont, string name, string chatter, string moveType, Color color)
         {
             _gremlinSprite = gremlinSprite;
             _gremlinFont = gremlinFont;
             _name = name;
+            _color = color;
 
             if (chatter != "" || chatter != string.Empty)
-            {
                 chatter = "\"" + chatter + "\"";
-            }
+            
             _chatter = chatter;
 
+            
             _rows = 5;
             _cols = 16;
             _currentFrame = 0;
 
-            _animRow = 1;
-            _endCol = 8;
+            if (moveType == "pace" || moveType == "circle" || moveType == "random" || moveType == "chase" || moveType == "bounce" || moveType == "path") 
+            {
+                _animRow = 1;
+                _endCol = 8;
+            }
+            else
+            {
+                _animRow = 0;
+                _endCol = 16;
+            }
 
-            _direction = new Vector2(0,0);
-            _position = new Vector2(200, 100);
+            _path = new List<Vector2>();
+            _destination = -1;
+
+            _position = new Vector2(0, 0);
+            _direction = new Vector2(0, 0);
+
             _speed = 100f;
             _scale = 3.0f;
 
+            _circularAngle = 0f;
 
             //walk variables
             _paceLeft = true;
+            _moveType = moveType;
+            _chaseTarget = _position;
+        }
+
+        public void SetStartPosition(Vector2 position)
+        {
+            _position = position;
+        }
+
+        public void SetChaseTarget(Vector2 chaseMe)
+        {
+            _chaseTarget = chaseMe;
+        }
+
+        public Vector2 GetPosition()
+        {
+            return _position;
+        }
+
+        public void SetChatter(string chatter)
+        {
+            _chatter = chatter;
+        }
+
+        public string GetChatter() { return _chatter; }
+
+        public string GetName() { return _name; }
+
+        public void SetDirection(Vector2 dir) { _direction = dir; }
+
+        public void SetPath() { 
+            _path.Add(new Vector2(400, 400));
+            _path.Add(new Vector2(700, 400));
+            _path.Add(new Vector2(700, 475));
+            _path.Add(new Vector2(400, 475));
         }
 
         public void Update(GameTime gameTime)
@@ -61,23 +124,88 @@ namespace GremlinWalks
                     _currentFrame = 0;
             }
 
-            pace(new Vector2(50,100), new Vector2(250,100), gameTime);
+            if (_moveType == "pace")
+                pace(new Vector2(50, 100), new Vector2(250, 100), gameTime);
+            else if (_moveType == "random")
+                RandomWalk(gameTime);
+            else if (_moveType == "chase")
+                move(_chaseTarget, gameTime);
+            else if (_moveType == "bounce")
+                bounce(gameTime);
+            else if (_moveType == "path" || _moveType == "circle")
+                path(gameTime);
+        }
+
+        //TODO: There are some issues in here, it's changing too frequently.
+        public void RandomWalk(GameTime gameTime)
+        {
+            Random rng  = new Random();
+            randomHTimeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
+            if (randomHTimeSinceLastFrame > randomHMillisecondsPerFrame)
+            {
+                randomHTimeSinceLastFrame -= randomHMillisecondsPerFrame;
+                randomDeltaX = rng.Next((int)-_speed, (int)_speed);
+            }
+
+            randomVTimeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
+            if (randomVTimeSinceLastFrame > randomVMillisecondsPerFrame)
+            {
+                randomVTimeSinceLastFrame -= randomVMillisecondsPerFrame;
+                randomDeltaY = rng.Next((int)-_speed, (int)_speed);
+            }
+
+            if (randomDeltaX > 0)
+                _paceLeft = false;
+            else
+                _paceLeft = true;
+
+            if (_position.X < 50 || _position.X > 750)
+                randomDeltaX = -randomDeltaX;
+
+            if (_position.Y < 50 || _position.Y > 550)
+                randomDeltaY = -randomDeltaY;
+
+            _position.X += (randomDeltaX * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            _position.Y += (randomDeltaY * (float)gameTime.ElapsedGameTime.TotalSeconds);
+
+        }
+
+        public void SetCircle(float radius)
+        {
+            float tempX = _position.X;
+            float tempY = _position.Y;
+            for(float i = (float)-Math.PI; i <= (float)Math.PI; i += 0.1f)
+            {
+                tempX -= radius * (float)Math.Sin(i);
+                tempY += radius * (float)Math.Cos(i);
+                _path.Add(new Vector2(tempX, tempY));
+            }
+            /*
+            _circularAngle += (float)(gameTime.ElapsedGameTime.TotalSeconds * _speed/(radius * 25f));
+            
+            _position.X -= radius * (float)Math.Sin(_circularAngle);
+            _position.Y += radius * (float)Math.Cos(_circularAngle);
+            
+            if(radius * (float)Math.Sin(_circularAngle) < 1)
+                _paceLeft = false;
+            else 
+                _paceLeft = true; 
+            */
         }
 
         public void pace(Vector2 startPace, Vector2 endPace, GameTime gameTime)
         {
+
             if (_position.X <= startPace.X)
-            {
                 _paceLeft = false;
-            }
-            if(_position.X >= endPace.X)
-            {
+
+            if (_position.X >= endPace.X)
                 _paceLeft = true;
-            }
 
-            if(_paceLeft) { move(startPace, gameTime);  }
-            else { move(endPace, gameTime); }
-
+            if (_paceLeft) 
+                move(startPace, gameTime);  
+            else
+                move(endPace, gameTime); 
         }
 
         public void move(Vector2 target, GameTime gameTime)
@@ -89,7 +217,13 @@ namespace GremlinWalks
             float x = temp.X;
             float y = temp.Y;
 
+            if (_position.X > target.X)
+                _paceLeft = true;
+            else
+                _paceLeft = false;
+
             _position += new Vector2(x, y);
+
             //float xP, yP;
             //double angle = Math.Acos(((x * _direction.X) + (y * _direction.Y)) / (temp.Length() * _direction.Length())); //dot product finds the angle between temp and direction
             //xP = (float)(Math.Cos(angle) * (x - _direction.X) - Math.Sin(angle) * (y - _direction.Y) + x);
@@ -97,6 +231,51 @@ namespace GremlinWalks
             //return new Vector2(x, y);
         }
 
+
+        public void bounce(GameTime gameTime)
+        {
+            //adpated from: https://stackoverflow.com/questions/12715096/xna-vector-math-movement
+            //Vector2 temp = (_direction - _position); // gets the difference between the target and position
+            _direction.Normalize();                   // sets the vector to unit vector
+            //_direction = ;                  // sets the vector to be the length of moveSpeed
+            //float x = _direction.X;
+            //float y = _direction.Y;
+
+            if (_position.X <  50 || _position.X > 300)
+                _direction.X = -_direction.X;
+            if (_position.Y < 300 || _position.Y > 500)
+                _direction.Y = -_direction.Y;
+
+            if (_direction.X > 0)
+                _paceLeft = false;
+            else
+                _paceLeft = true;
+
+            _position += _direction * _speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            //float xP, yP;
+            //double angle = Math.Acos(((x * _direction.X) + (y * _direction.Y)) / (temp.Length() * _direction.Length())); //dot product finds the angle between temp and direction
+            //xP = (float)(Math.Cos(angle) * (x - _direction.X) - Math.Sin(angle) * (y - _direction.Y) + x);
+            //yP = (float)(Math.Sin(angle) * (x - _direction.X) - Math.Cos(angle) * (y - _direction.Y) + y);            // these lines rotate the point x,y around the direction vector by angle "angle"
+            //return new Vector2(x, y);
+        }
+
+        public void path(GameTime gameTime)
+        {
+            if(_destination == -1)
+            {
+                _destination = 0;
+            }
+            if (Vector2.Distance(_path[_destination], _position) < 5)
+            {
+                _destination++;
+            }
+            if(_destination >= _path.Count) { _destination = 0; }
+
+            SetChatter(_path[_destination] + "");
+
+            move(_path[_destination], gameTime);
+        }
 
         public void Draw(SpriteBatch spriteBatch)
         {
@@ -115,7 +294,7 @@ namespace GremlinWalks
 
 
             spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null);
-            spriteBatch.DrawString(_gremlinFont, _name, new Vector2(_position.X - (int)(_gremlinFont.MeasureString(_name).X / 2), _position.Y - 80), Color.Yellow);
+            spriteBatch.DrawString(_gremlinFont, _name, new Vector2(_position.X - (int)(_gremlinFont.MeasureString(_name).X / 2), _position.Y - 80), _color);
             spriteBatch.DrawString(_gremlinFont, _chatter, new Vector2(_position.X - (int)(_gremlinFont.MeasureString(_chatter).X / 2), _position.Y - 60), Color.White);
             spriteBatch.Draw(_gremlinSprite, _position, sourceRectangle, Color.White, 0, new Vector2(width/2, height/2), new Vector2(_scale, _scale), faceLeft, 0);
             spriteBatch.End();
